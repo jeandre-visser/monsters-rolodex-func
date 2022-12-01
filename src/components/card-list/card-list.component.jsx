5920f5f1c449984aa9214974f4d3ad572b1a40ae
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-30 20:01:31 -0700

    Update to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,24 +1,22 @@
-import { Component } from 'react';
 import './card-list.styles.css';
 import Card from '../card/card.component.jsx';
 
-class CardList extends Component {
-// whenever props change or update, then the component will rerender
-  render() {
-    const { monsters } = this.props;
+// props is exactly the same as this.props in classes
+// so instead of destructuring const { monster } = props; we can just pass in { monsters } as a CardList parameter directly
+const CardList = ({ monsters }) => {
 
-    return (
-      <div className='card-list'>
-        {
-          monsters.map((monster) => {
-            return (
-              <Card monster={monster} />
-            )
-          })
-        }
-      </div>
-    )
-  }
+
+  return (
+    <div className='card-list'>
+      {
+        monsters.map((monster) => {
+          return (
+            <Card monster={monster} />
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default CardList;
