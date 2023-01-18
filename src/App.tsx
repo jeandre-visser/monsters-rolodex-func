4478c288427a9cf27dca9da7d61524480c6916d8
Jeandre Visser <jeandrev1414@gmail.com>
2023-01-17 21:32:12 -0700

    Type search box and create a getData helper function

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
+import { getData } from './utils/data.utils';
 
+type Monster = {
+  id: string;
+  name: string;
+  email: string;
+}
 
 const App = () => {
 
   const [searchField, setSearchField] = useState('a');
-  const [monsters, setMonsters] = useState([]);
+  const [monsters, setMonsters] = useState<Monster[]>([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then((users) => setMonsters(users)
-    )
-    // dependency array is empty because we do not want to trigger this useEffect again, We only want to trigger it on mount
+    const fetchUsers = async () => {
+      const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users');
+      setMonsters(users);
+    }
+    fetchUsers();
   }, [])
 
   useEffect(() => {
@@ -28,7 +34,7 @@ const App = () => {
   }, [monsters, searchField]);
   
   
-  const onSearchChange = (event) => {
+  const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
   // Lowercase search input
   const searchFieldString = event.target.value.toLocaleLowerCase();
   setSearchField(searchFieldString);
